e4bfee138ed42a1119b1b6a6bdc24f845092c092
Jeriel <jerielweb@outlook.com>
2025-09-23 15:41:18 -0600

    mejoramiento de ilustracion

---
+++ scripts/nav.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', () => {
+      function toggleMenu() {
+    hamburgerBtn.classList.toggle('active')
+    navMenu.classList.toggle('active')
+    navOverlay.classList.toggle('active')
+    
+    if (navMenu.classList.contains('active')) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'auto'
+    }
+  }
+  
+ 
+  function closeMenu() {
+    hamburgerBtn.classList.remove('active')
+    navMenu.classList.remove('active')
+    navOverlay.classList.remove('active')
+    document.body.style.overflow = 'auto'
+  }
+  
+  hamburgerBtn.addEventListener('click', toggleMenu)
+  navOverlay.addEventListener('click', closeMenu);
+  
+  const navMenuLinks = document.querySelectorAll('.nav-menu a')
+  navMenuLinks.forEach(link => {
+    link.addEventListener('click', closeMenu)
+  });
+  
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 900) {
+      closeMenu()
+    }
+  })
+})
