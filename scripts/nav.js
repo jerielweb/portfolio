7fb0d8cea632851391c51e703c21716849056680
Jeriel Beckford <jerielweb@otlook.es>
2025-09-27 15:23:09 -0600

    Se agrego el event de cierre fuera de la ventana abierta

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerBtn = document.getElementById('hamburgerBtn');
-    const navMenu = document.getElementById('navMenu');
-    // const navOverlay = document.getElementById('navOverlay');
+    const hamburgerBtn = document.getElementById('hamburgerBtn')
+    const navMenu = document.getElementById('navMenu')
     hamburgerBtn.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-        hamburgerBtn.classList.toggle('active');
-        // navOverlay.classList.toggle('active');
-    });
+        navMenu.classList.toggle('active')
+        hamburgerBtn.classList.toggle('active')
+        
+    })
+    document.addEventListener('click', (event) => {
+        if (!navMenu.contains(event.target) && !hamburgerBtn.contains(event.target)) {
+            navMenu.classList.remove('active')
+            hamburgerBtn.classList.remove('active')
+        }
+    })   
 })
